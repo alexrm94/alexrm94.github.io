8b164289c6698783449c8302b2f865921223dbcf
alexrm94 <alex.rm94@hotmail.com>
2023-03-03 22:32:06 -0300

    v7
    v7

---
+++ index.js
@@ -0,0 +1,105 @@
+var r = document.querySelector(':root');
+
+const farm = document.getElementById("farm");
+const product = document.getElementById("product");
+const video = document.getElementById("video");
+const team = document.getElementById("team");
+const contact = document.getElementById("contact");
+const nav1 = document.getElementById("nav1");
+const nav2 = document.getElementById("nav2");
+const nav3 = document.getElementById("nav3");
+const nav4 = document.getElementById("nav4");
+const nav5 = document.getElementById("nav5");
+
+
+function english() {
+    farm.style.fontFamily = "Love"
+    product.style.fontFamily = "Love";
+    video.style.fontFamily = "Love";
+    team.style.fontFamily = "Love";
+    contact.style.fontFamily = "Love";
+    nav1.style.fontFamily = "Love";
+    nav2.style.fontFamily = "Love";
+    nav3.style.fontFamily = "Love";
+    nav4.style.fontFamily = "Love";
+    nav5.style.fontFamily = "Love";
+    
+   
+    farm.innerHTML = "Know the Farm";
+    product.innerHTML = "Pineapple";
+    video.innerHTML = "Watch our Content";
+    team.innerHTML = "Know our team";
+    contact.innerHTML = "Contact Information";
+    nav1.innerHTML = "Farm";
+    nav2.innerHTML = "Pineapple";
+    nav3.innerHTML = "Videos";
+    nav4.innerHTML = "Team";
+    nav5.innerHTML = "Contacts";
+  
+
+
+
+}
+
+
+function chinese() {
+    farm.style.fontFamily = "Chinese";
+    product.style.fontFamily = "Chinese";
+    video.style.fontFamily = "Chinese";
+    team.style.fontFamily = "Chinese";
+    contact.style.fontFamily = "Chinese";
+    nav1.style.fontFamily = "Chinese";
+    nav2.style.fontFamily = "Chinese";
+    nav3.style.fontFamily = "Chinese";
+    nav4.style.fontFamily = "Chinese";
+    nav5.style.fontFamily = "Chinese";
+   
+    farm.innerHTML = "了解农场";
+    product.innerHTML = "菠萝";
+    video.innerHTML = "观看我们的内容";
+    team.innerHTML = "了解我们的团队";
+    contact.innerHTML = "联系信息";
+    nav1.innerHTML = "农场";
+    nav2.innerHTML = "菠萝";
+    nav3.innerHTML = "影片";
+    nav4.innerHTML = "团队";
+    nav5.innerHTML = "联系人";
+}
+
+function khmer() {
+
+    const element = document.getElementById("farm");
+    document.getElementById("farm").style.fontFamily = "khmer";
+    document.getElementById("product").style.fontFamily = "khmer";
+    document.getElementById("video").style.fontFamily = "khmer";
+    document.getElementById("team").style.fontFamily = "khmer";
+    document.getElementById("contact").style.fontFamily = "khmer";
+    element.innerHTML = "ស្គាល់កសិដ្ឋានរបស់យើង។";
+
+
+    farm.style.fontFamily = "khmer";
+    product.style.fontFamily = "khmer";
+    video.style.fontFamily = "khmer";
+    team.style.fontFamily = "khmer";
+    contact.style.fontFamily = "khmer";
+    nav1.style.fontFamily = "khmer";
+    nav2.style.fontFamily = "khmer";
+    nav3.style.fontFamily = "khmer";
+    nav4.style.fontFamily = "khmer";
+    nav5.style.fontFamily = "khmer";
+   
+    farm.innerHTML = "ស្គាល់កសិដ្ឋាន";
+    product.innerHTML = "ម្នាស់";
+    video.innerHTML = "មើលមាតិការបស់យើង។";
+    team.innerHTML = "ស្គាល់ក្រុមរបស់យើង។";
+    contact.innerHTML = "ព័ត៌មានទំនាក់ទំនង";
+    nav1.innerHTML = "កសិដ្ឋាន";
+    nav2.innerHTML = "ម្នាស់";
+    nav3.innerHTML = "វីដេអូ";
+    nav4.innerHTML = "ក្រុម";
+    nav5.innerHTML = "ទំនាក់ទំនង";
+
+}
+
+
+
